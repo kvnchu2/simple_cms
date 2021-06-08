f638baf94eb0be3d8ea06d54e75ceba6bdb9dc95
Kevin Chu <kvnchu2@gmail.com>
2021-06-08 00:56:52 +0000

    add contact and about features

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -7,5 +7,23 @@ class DemoController < ApplicationController
 
   def hello
     @array = [1,2,3,4,5]
+
+    @id= params['id']
+    @page = params[:page]
+  end
+
+  def about
+    render('about_us')
+  end
+
+  def contact
+    if params['country'] == 'us'
+      @phone = '(800) 555-6789'
+    elsif params['country'] == 'uk'
+      @phone = '(020) 7946 1234'
+    else
+      @phone = '+1 (987) 654-3210'
+    end
+    render('contact_us')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'demo#index'
   get 'demo/index'
   get 'demo/hello'
+  get 'demo/contact'
+  get 'demo/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #default route
